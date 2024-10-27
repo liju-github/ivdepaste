b9a2159a157fc2a909ae934c891e1e8e048c0c67
liju-github <lijuthomasliju03@gmail.com>
2024-10-27 21:25:54 +0530

    removed csr in layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-'use client'
+
 import { AuthProvider } from "../providers/auth-provider";
 import { Navbar } from "@/src/components/header/Navbar";
 import "./globals.css";
 import { Toaster } from "@/src/components/ui/toaster";
 import { ThemeWrapper } from "../components/ui/theme";
 import ThemeButton from "../components/ui/themebutton";
-import { Provider } from "react-redux";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
